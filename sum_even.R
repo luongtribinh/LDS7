6630f4bc9b82c777475efe29bb6e60a4771627b7
luongtribinh <70083959+luongtribinh@users.noreply.github.com>
2020-08-23 10:40:20 +0700

    Create sum_even.R

---
+++ sum_even.R
@@ -0,0 +1,14 @@
+# 1. What?
+# 2. Solve?
+# 3. Input?
+# 4. Output? Print or return
+
+sum_even.function <- function(start, end){
+  sum_even <- 0
+  for (i in start:end){
+    if (i%%2==0){
+      sum_even <- sum_even + i
+    }
+  }
+  return(sum_even)
+}
